d7a063cd9be3590e116737d94ad502cddfce3ecf
MatthewNguyenAYJ <42983707+MatthewNguyenAYJ@users.noreply.github.com>
2018-11-29 15:25:32 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,23 @@
+// Daniel Shiffman
+// https://www.kadenze.com/courses/the-nature-of-code
+// http://natureofcode.com/
+// Session 2: Array of Particles, multiple forces
+
+
+var person;
+//var obstacle;
+
+function setup() {
+  createCanvas(640, 360);
+	person =  new Person();
+}
+
+
+
+
+
+function draw() {
+background(51);
+person.update();
+person.display();
+}
